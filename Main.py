6a3087abe9b181fde731abed0b12a8ba9e409134
tran trung hieu <trantrunghieu7492@gmail.com>
2016-01-19 02:32:47 +0300

    Created some basical functions to analysis sentiment

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 from tkinter import *
 import tkinter.messagebox
 
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import subjectivity
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 # Main function in program
 def center(toplevel):
     toplevel.update_idletasks()
@@ -15,8 +21,31 @@ def callback():
     if tkinter.messagebox.askokcancel("Quit", "Do you really wish to quit?"):
         main.destroy()
 
+def setResult(type, res):
+    if (type == "neg"):
+        negativeLabel.configure(text = "Negative : " + str(res) + " % \n")
+    if (type == "neu"):
+        neutralLabel.configure( text = "Neutral  : " + str(res) + " % \n")
+    if (type == "pos"):
+        positiveLabel.configure(text = "Positive : " + str(res) + " % \n")
+
 def runAnalysis():
-    typedText.configure(text = "Your text: " + line.get())
+    sentences = []
+    sentences.append(line.get())
+    sid = SentimentIntensityAnalyzer()
+    for sentence in sentences:
+        # print(sentence)
+        ss = sid.polarity_scores(sentence)
+        for k in sorted(ss):
+            setResult(k, ss[k])
+                # print('{0}: {1} \n'.format(k, ss[k]), end='')
+    print()
+
+def editedText(event):
+    typedText.configure(text = "Your text:\n" + line.get() + event.char)
+
+def runByEnter(event):
+    runAnalysis()
 
 # Create main window
 main = Tk()
@@ -31,22 +60,26 @@ center(main)
 label1 = Label(text = "Type your review:")
 label1.pack()
 
-def typeKeyReturn(event):
-    runAnalysis()
 
 line = Entry(main, width=70)
-line.bind("<Return>",typeKeyReturn)
-line.bind("<Key>",typeKeyReturn)
+line.bind("<Key>",editedText)
+line.bind("<Return>",runByEnter)
 line.pack()
 
-typedText = Label(text = "Your text :")
+typedText = Label(text = "Your text:\n")
 typedText.pack()
 
-analysisButton = Button(main, text = "Analysis", width=10, command = runAnalysis)
-analysisButton.grid(row=50, column=550)
-analysisButton.pack()
+# analysisButton = Button(main, text = "Analysis", width=10, command = runAnalysis)
+# analysisButton.grid(row=50, column=550)
+# analysisButton.pack()
 
-
-
+result = Label(text = "\nResult\n")
+result.pack()
+negativeLabel = Label(text = "")
+negativeLabel.pack()
+neutralLabel  = Label(text = "")
+neutralLabel.pack()
+positiveLabel = Label(text = "")
+positiveLabel.pack()
 # Run program
 mainloop()
